29a8c1d2b7c607697bd8e226e2436f13eb156012
vitorramos <ramos.vitor89@gmail.com>
2022-06-05 16:58:40 -0300

    aptx option

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 CXX = g++
 LIBS = -lpulse-simple -lpulse -lpthread -ldbus-1
 INCLUDE = -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include
+FILES = server_pulse.cpp pa_utils.cpp dbus_utils.cpp openaptx.c
 
-server_pulse : server_pulse.cpp pa_utils.cpp dbus_utils.cpp openaptx.c
-	$(CXX) -O3 $(INCLUDE) $? $(LIBS) -o $@ 
+server_pulse: $(FILES)
+	$(CXX) -O3 $(INCLUDE) $(FILES) $(LIBS) -o $@ 
 
 install:
 	cp server_pulse /usr/bin/server_pulse
--- server_pulse.cpp
+++ server_pulse.cpp
@@ -81,6 +81,7 @@ int main(int argc, char** argv)
     if(argc == 2){
         if(strcmp(argv[1], "--aptx") == 0){
             with_aptx = true;
+            cout << "aptx enabled" << endl;
         }
     }
     thread reciver(recv_server_addr);
@@ -106,6 +107,7 @@ int main(int argc, char** argv)
     if(name == ""){
         name = "";
     }
+    cout << "output " << name << endl;
     s = pa_simple_new(NULL,               // Use the default server.
                     "pc relay",           // Our application's name.
                     PA_STREAM_RECORD,
@@ -118,31 +120,18 @@ int main(int argc, char** argv)
                     );
     int error;
     uint8_t buffer[2048];
-    uint8_t output_buffer[2048/4];
-    uint8_t decoded_buffer[2048];
+    uint8_t output_buffer[512];
 
-    int hd = 0;
-    int ret;
-    size_t length;
     size_t processed;
     size_t written;
-    size_t dropped;
-    int synced;
-    int syncing;
 
     struct aptx_context *ctx_enc;
-    struct aptx_context *ctx_dec;
-    ctx_enc = aptx_init(hd);
+    ctx_enc = aptx_init(0);
     if (!ctx_enc) {
         fprintf(stderr, "Cannot initialize aptX encoder\n");
         return 1;
     }
-    ctx_dec = aptx_init(hd);
-    if (!ctx_dec) {
-        fprintf(stderr, "Cannot initialize aptX encoder\n");
-        return 1;
-    }
-
+    int n;
     while(1){
         if(pa_simple_read(s, buffer, sizeof(buffer), &error) < 0){
             break;
@@ -153,49 +142,15 @@ int main(int argc, char** argv)
             cout << "Latency " << latency << endl;
         uint8_t sum = 0;
         // for(int i=0; i<2048; i++) sum |= buffer[i];
-        // if(sum != 0)
-        processed = aptx_encode(ctx_enc, buffer, sizeof(buffer), output_buffer, sizeof(output_buffer), &written);
-        if (processed != sizeof(buffer))
-            break;
-        // processed = aptx_decode_sync(ctx_dec, output_buffer, sizeof(output_buffer), decoded_buffer, sizeof(decoded_buffer), &written, &synced, &dropped);
-        // /* Check all possible states of synced, syncing and dropped status */
-        // if (!synced) {
-        //     if (!syncing) {
-        //         fprintf(stderr, "aptX decoding failed, synchronizing\n");
-        //         syncing = 1;
-        //         ret = 1;
-        //     }
-        //     if (dropped) {
-        //         fprintf(stderr, "aptX synchronization successful, dropped %lu byte%s\n", (unsigned long)dropped, (dropped != 1) ? "s" : "");
-        //         syncing = 0;
-        //         ret = 1;
-        //     }
-        //     if (!syncing) {
-        //         fprintf(stderr, "aptX decoding failed, synchronizing\n");
-        //         syncing = 1;
-        //         ret = 1;
-        //     }
-        // } else {
-        //     if (dropped) {
-        //         if (!syncing)
-        //             fprintf(stderr, "aptX decoding failed, synchronizing\n");
-        //         fprintf(stderr, "aptX synchronization successful, dropped %lu byte%s\n", (unsigned long)dropped, (dropped != 1) ? "s" : "");
-        //         syncing = 0;
-        //         ret = 1;
-        //     } else if (syncing) {
-        //         fprintf(stderr, "aptX synchronization successful\n");
-        //         syncing = 0;
-        //         ret = 1;
-        //     }
-        // }
-        // /* If we have not decoded all supplied samples then decoding unrecoverable failed */
-        // if (processed != sizeof(output_buffer)) {
-        //     fprintf(stderr, "aptX decoding failed\n");
-        //     ret = 1;
-        //     break;
-        // }
-        // written -= 2*2;
-        int n = sendto(sockfd, output_buffer, sizeof(output_buffer), 0, (sockaddr*)&cliaddr, sizeof(cliaddr));
+        // if(sum == 0) continue
+        if(with_aptx){
+            processed = aptx_encode(ctx_enc, buffer, sizeof(buffer), output_buffer, sizeof(output_buffer), &written);
+            if (processed != sizeof(buffer))
+                break;
+            n = sendto(sockfd, output_buffer, sizeof(output_buffer), 0, (sockaddr*)&cliaddr, sizeof(cliaddr));
+        }
+        else
+            n = sendto(sockfd, buffer, sizeof(buffer), 0, (sockaddr*)&cliaddr, sizeof(cliaddr));
     }
     reciver.join();
     cmds.join();
